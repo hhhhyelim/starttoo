21f6aeacf242844c14463ba226c67b8eb22e5525
simjaehoon <simjaehoon3@naver.com>
2026-07-22 15:40:28 +0900

    [FE] test: 권한 테스트

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 function App() {
-  return <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind</h1>;
+  return <h1 className="text-3xl font-bold text-blue-500">HelloWorldddd Tailwind</h1>;
 }
+
 
 export default App;
